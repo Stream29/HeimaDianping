f65279214d3d67f646905efdb7a1175a9fe12476
Stream <1542763342@qq.com>
2024-08-26 06:16:40 +0800

    feat: 基于Redis实现短信登录

--- src/main/java/com/hmdp/service/impl/UserServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.util.RandomUtil;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.hmdp.dto.LoginFormDTO;
@@ -10,10 +11,18 @@ import com.hmdp.mapper.UserMapper;
 import com.hmdp.service.IUserService;
 import com.hmdp.utils.RegexUtils;
 import lombok.extern.slf4j.Slf4j;
+import lombok.val;
+import lombok.var;
 import org.jetbrains.annotations.NotNull;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpSession;
+
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
+
+import static com.hmdp.utils.RedisConstants.*;
 
 /**
  * <p>
@@ -26,6 +35,12 @@ import javax.servlet.http.HttpSession;
 @Service
 @Slf4j
 public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IUserService {
+
+    private final StringRedisTemplate stringRedisTemplate;
+
+    public UserServiceImpl(StringRedisTemplate stringRedisTemplate) {
+        this.stringRedisTemplate = stringRedisTemplate;
+    }
 
     @Override
     public @NotNull Result sendCode(@NotNull String phone, @NotNull HttpSession session) {
@@ -48,33 +63,35 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IU
     @Override
     public @NotNull Result login(@NotNull LoginFormDTO loginForm, @NotNull HttpSession session) {
         // 1.校验手机号
-        String phone = loginForm.getPhone();
+        val phone = loginForm.getPhone();
         if (RegexUtils.isPhoneInvalid(phone)) {
             // 2.如果不符合，返回错误信息
             return Result.fail("手机号格式错误！");
         }
         // 3.校验验证码
-        Object cacheCode = session.getAttribute("code");
-        String code = loginForm.getCode();
-        if (cacheCode == null || !cacheCode.toString().equals(code)) {
+        val cacheCode =
+                stringRedisTemplate.opsForValue()
+                        .get(LOGIN_CODE_KEY + phone);
+        val code = loginForm.getCode();
+        if (cacheCode == null || !cacheCode.equals(code)) {
             //3.不一致，报错
             return Result.fail("验证码错误");
         }
         //一致，根据手机号查询用户
-        User user = query().eq("phone", phone).one();
+        var user = query().eq("phone", phone).one();
 
         //5.判断用户是否存在
         if (user == null) {
             //不存在，则创建
             user = new User().setPhone(phone);
         }
-        //7.保存用户信息到session中
-        session.setAttribute("user",
-                UserDTO.builder()
-                        .id(user.getId())
-                        .nickName(user.getNickName())
-                        .icon(user.getIcon())
-                        .build());
+        val userDTO = BeanUtil.copyProperties(user, UserDTO.class);
+
+        val tokenKey = LOGIN_USER_KEY + UUID.randomUUID();
+
+        stringRedisTemplate.opsForHash().putAll(tokenKey, BeanUtil.beanToMap(userDTO));
+
+        stringRedisTemplate.expire(tokenKey, LOGIN_USER_TTL, TimeUnit.MINUTES);
 
         return Result.ok();
     }
